91a507c8e5aeea0ebb83e259792deec148ff0786
Joel.Hutson <joel.hutson@cmd.com.au>
2022-08-15 17:34:53 +1000

    updating variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "event_rules" {
   type        = list(any)
   description = "Details of event rules."
+  presence = required
 }
 
 variable "workspace_ids"{
   type        = string
   description = "account for lambda function env"
+  presence = required
 }
 
 variable "region"{
   type        = string
   description = "region for ps script"
+  presence = required
 }
